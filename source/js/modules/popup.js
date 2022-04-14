d3f371cea16e6d791dce2396f1a6b20daa88311c
jein-sh <jein.sh85@gmail.com>
2022-04-14 21:42:17 +0200

    Правки после тестирования

--- source/js/modules/popup.js
+++ source/js/modules/popup.js
@@ -3,6 +3,7 @@ let buttonHeader = document.querySelector('.header__button');
 let modal = document.querySelector('.modal');
 let modalClose = modal.querySelector('.modal__close');
 let inputName = modal.querySelector('#modal-name');
+
 let overlay = document.querySelector('.overlay');
 
 buttonHeader.addEventListener('click', function () {
@@ -25,12 +26,30 @@ overlay.addEventListener('click', function () {
 });
 
 window.addEventListener('keydown', function (evt) {
-  if (evt.key === 'Esc' || evt.key === 'Escape') {
-    if (modal.classList.contains('modal--show')) {
-      evt.preventDefault();
-      modal.classList.remove('modal--show');
-      overlay.classList.remove('overlay--show');
-      body.classList.remove('scroll-lock');
+
+  let firstTabStop = inputName;
+  let lastTabStop = modalClose;
+
+  if (evt.keyCode === 9) {
+    if (evt.shiftKey) {
+      if (document.activeElement === firstTabStop) {
+        evt.preventDefault();
+        lastTabStop.focus();
+      }
+    } else {
+      if (document.activeElement === lastTabStop) {
+        evt.preventDefault();
+        firstTabStop.focus();
+      }
+    }
+
+    if (evt.key === 'Esc' || evt.key === 'Escape') {
+      if (modal.classList.contains('modal--show')) {
+        evt.preventDefault();
+        modal.classList.remove('modal--show');
+        overlay.classList.remove('overlay--show');
+        body.classList.remove('scroll-lock');
+      }
     }
   }
 });
